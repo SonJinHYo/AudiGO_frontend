c67baa029e28de267f0d4833b7e9f3a9f4c9bd37
SonJinHYo <wlsgy95@naver.com>
2023-07-28 10:32:26 +0900

    react 개발 환경 설정. react(ts), chakraUI, react-router-dom 설치

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div>
+      <Text>test</Text>
+    </div>
+  );
+}
+
+export default App;
